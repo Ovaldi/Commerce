cdb47194c31d89ad7334df792f78f2dcd5ba51a3
Jifeng <jifeng@kooboo.com>
2013-05-27 12:38:15 +0800

    Kooboo Commerce solution

---
+++ AssemblyInfoGlobal.cs
@@ -0,0 +1,16 @@
+﻿#region License
+// 
+// Copyright (c) 2013, Kooboo team
+// 
+// Licensed under the BSD License
+// See the file LICENSE.txt for details.
+// 
+#endregion
+using System.Reflection;
+[assembly: AssemblyProduct("Kooboo.Commerce")]
+[assembly: AssemblyCompany("Yardi Technology Limited")]
+[assembly: AssemblyCopyright("Copyright © Yari 2009-2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0527")]
+[assembly: AssemblyFileVersion("1.0.0.0527")]
